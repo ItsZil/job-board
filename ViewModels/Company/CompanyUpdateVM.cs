5e986221565e5dc6400b7bc8a5551b70dfa78f38
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-11-29 01:04:36 +0200

    Update company

--- ViewModels/Company/CompanyUpdateVM.cs
+++ ViewModels/Company/CompanyUpdateVM.cs
@@ -5,16 +5,18 @@ namespace job_board.ViewModels.Company
     public class CompanyUpdateVM
     {
         [Required]
-        [StringLength(100)]
+        [StringLength(75)]
         public string Company { get; set; }
 
+        [Required]
+        [StringLength(1000)]
         public string? CompanyDescription { get; set; }
 
         [Required]
         [StringLength(50)]
         public string Industry { get; set; }
 
-        [StringLength(255)]
+        [StringLength(50)]
         public string? Website { get; set; }
     }
 }
