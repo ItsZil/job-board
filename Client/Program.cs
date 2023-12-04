7a79040a453f0663af86545e1986ebd3e07f62e6
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-12-04 18:59:07 +0200

    Convert to WebAssembly app

---
+++ Client/Program.cs
@@ -0,0 +1,33 @@
+using job_board.Client;
+using job_board.Utilities;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddMudServices();
+        //builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<TokenAuthStateProvider>();
+        builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<TokenAuthStateProvider>());
+
+        builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        var baseAddress = "https://localhost:7142";//builder.Configuration["Base_Address"];
+        builder.Services.AddHttpClient("httpClient", client =>
+        {
+            client.BaseAddress = new Uri(baseAddress);
+        });
+
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("httpClient"));
+
+        await builder.Build().RunAsync();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,87 @@
+using job_board.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MudBlazor.Services;
+using System.Text;
+
+internal class Program
+{
+    public static WebApplication Application { get; set; }
+
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddControllers();
+        builder.Services.AddMudServices();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<TokenAuthStateProvider>();
+        builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<TokenAuthStateProvider>());
+
+        var baseAddress = builder.Configuration["Base_Address"];
+        builder.Services.AddHttpClient("httpClient", client =>
+        {
+            client.BaseAddress = new Uri(baseAddress);
+        });
+
+        var connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connection,
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(10), errorNumbersToAdd: null);
+                }));
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt_Issuer"],
+                    ValidAudience = builder.Configuration["Jwt_Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt_Key"]))
+                };
+            });
+
+        builder.Services.AddScoped<DbHelper>();
+
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("httpClient"));
+
+        var app = builder.Build();
+        Application = app;
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
+}
